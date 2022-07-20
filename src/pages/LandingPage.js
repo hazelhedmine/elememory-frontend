1c1177a22dbb2f2f357aaa31dd9a26ed6418e0a7
Hazel Tan <e0424860@u.nus.edu>
2022-07-20 16:12:31 +0800

    Wrap body contents in landing page

--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -1,16 +1,19 @@
-import { Box } from '@chakra-ui/react'
+import { Box, Flex, Heading } from '@chakra-ui/react'
 import Card from 'components/card/Card'
 import LandingPageLayout from 'layouts/LandingPageLayout'
 
 const LandingPage = props => {
   return (
     <LandingPageLayout>
-      <Box h="100%" textAlign="center" fontSize="xl">
-        <Card
-          question="This is where your question or prompt is!"
-          answer="This is where your answer will be!"
-        ></Card>
-      </Box>
+      <Flex direction={'column'} align={'center'} gap={24}>
+        <Heading>Try out the card below!</Heading>
+        <Box textAlign="center" fontSize="xl">
+          <Card
+            question="This is where your question or prompt is!"
+            answer="This is where your answer will be!"
+          ></Card>
+        </Box>
+      </Flex>
     </LandingPageLayout>
   )
 }
