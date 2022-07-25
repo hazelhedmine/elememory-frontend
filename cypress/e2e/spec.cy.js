62b1f67b919953bad769d7e079bd35d63eacc6a4
Hazel Tan <e0424860@u.nus.edu>
2022-07-25 17:41:55 +0800

    Add eslint-plugin-cypress

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -1,5 +1,7 @@
-describe('empty spec', () => {
-  it('passes', () => {
-    cy.visit('https://example.cypress.io')
+describe('Note app', function () {
+  it('front page can be opened', function () {
+    cy.visit('http://localhost:3000')
+    cy.contains('Try out the card below!')
+    cy.contains('This is where your question or prompt is!')
   })
 })
