86de91f4e4717e482c49b2799ad42c8efd8811c8
Hazel Tan <e0424860@u.nus.edu>
2022-07-29 17:09:36 +0800

    Change background to light yellow in home page

--- src/layouts/HomePageLayout.js
+++ src/layouts/HomePageLayout.js
@@ -1,22 +1,24 @@
-import { Flex } from '@chakra-ui/react'
+import { Box, Flex, useColorModeValue } from '@chakra-ui/react'
 import Footer from 'components/footer/Footer'
 import Header from 'components/header/Header'
 
 const HomePageLayout = ({ user, removeUser, ...props }) => {
   return (
-    <Flex
-      direction="column"
-      justify="space-between"
-      align="center"
-      maxW={{ xl: '1200px' }}
-      m="0 auto"
-      minH="100vh"
-      {...props}
-    >
-      <Header user={user} removeUser={removeUser}></Header>
-      {props.children}
-      <Footer></Footer>
-    </Flex>
+    <Box bg={useColorModeValue('yellow.50')}>
+      <Flex
+        direction="column"
+        justify="space-between"
+        align="center"
+        maxW={{ xl: '1200px' }}
+        m="0 auto"
+        minH="100vh"
+        {...props}
+      >
+        <Header user={user} removeUser={removeUser}></Header>
+        {props.children}
+        <Footer></Footer>
+      </Flex>
+    </Box>
   )
 }
 
