ac4041463de9fa4b8da874454687acf002aad3ec
Hazel Tan <e0424860@u.nus.edu>
2022-09-19 17:52:43 +0800

    Update card table formatting in deck page

--- src/pages/DeckPage.js
+++ src/pages/DeckPage.js
@@ -1,10 +1,13 @@
-import { EditIcon } from '@chakra-ui/icons'
+import { AddIcon, EditIcon } from '@chakra-ui/icons'
 import {
+  Button,
+  Flex,
   Heading,
   IconButton,
   Table,
   TableContainer,
   Tbody,
+  Td,
   Th,
   Thead,
   Tr,
@@ -16,6 +19,16 @@ import HomePageLayout from 'layouts/HomePageLayout'
 import { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom'
 import deckService from 'services/decks'
+
+const DeleteButton = () => {
+  return (
+    <Td isNumeric>
+      <Button colorScheme={'red'} variant={'ghost'} size={'xs'}>
+        DELETE
+      </Button>
+    </Td>
+  )
+}
 
 const DeckPage = ({ storage, removeStorage, getToken }) => {
   const params = useParams() //deckId is params.id
@@ -37,49 +50,70 @@ const DeckPage = ({ storage, removeStorage, getToken }) => {
 
   return (
     <HomePageLayout storage={storage} removeStorage={removeStorage}>
-      <Heading
-        as={'h1'}
-        size={'2xl'}
-        textAlign={{ base: 'center', md: 'left' }}
-      >
-        {deckName}{' '}
-        <IconButton
-          colorScheme={'blackAlpha'}
-          variant={'ghost'}
-          aria-label="Edit deck name"
-          icon={<EditIcon></EditIcon>}
-          onClick={onOpen}
-        ></IconButton>
-        <DeckNameModal
-          id={params.id}
-          label={'Deck Name'}
-          name={deckName}
-          setName={setDeckName}
-          isOpen={isOpen}
-          onClose={onClose}
-        ></DeckNameModal>
-      </Heading>
+      <Flex direction={'column'} justify="center">
+        <Flex direction={'column'} align={'left'} m={10} gap={2}>
+          <Heading
+            as={'h1'}
+            size={'2xl'}
+            textAlign={{ base: 'center', md: 'left' }}
+          >
+            {deckName}{' '}
+            <IconButton
+              colorScheme={'blackAlpha'}
+              variant={'ghost'}
+              aria-label="Edit deck name"
+              icon={<EditIcon></EditIcon>}
+              onClick={onOpen}
+            ></IconButton>
+            <DeckNameModal
+              id={params.id}
+              label={'Deck Name'}
+              name={deckName}
+              setName={setDeckName}
+              isOpen={isOpen}
+              onClose={onClose}
+            ></DeckNameModal>
+          </Heading>
+        </Flex>
 
-      <TableContainer
-        borderRadius={'2xl'}
-        w={'92%'}
-        bg={useColorModeValue('white', 'gray.900')}
-        p={4}
-      >
-        <Table>
-          <Thead>
-            <Tr>
-              <Th>name</Th>
-            </Tr>
-          </Thead>
+        <Flex justify="center" mb={6}>
+          <Button
+            rightIcon={<AddIcon />}
+            size="md"
+            colorScheme="yellow"
+            variant="solid"
+          >
+            Create Deck
+          </Button>
+        </Flex>
 
-          <Tbody>
-            {cards.map(card => (
-              <Tr key={card.id}>{card.question}</Tr>
-            ))}
-          </Tbody>
-        </Table>
-      </TableContainer>
+        <Flex justify={'center'}>
+          <TableContainer
+            borderRadius={'2xl'}
+            w={'92%'}
+            bg={useColorModeValue('white', 'gray.900')}
+            p={4}
+          >
+            <Table>
+              <Thead>
+                <Tr>
+                  <Th>Question</Th>
+                  <Th>Answer</Th>
+                  <Th></Th>
+                </Tr>
+              </Thead>
+              <Tbody>
+                {cards.map(card => (
+                  <Tr key={card.id}>
+                    {card.question}
+                    <DeleteButton></DeleteButton>
+                  </Tr>
+                ))}
+              </Tbody>
+            </Table>
+          </TableContainer>
+        </Flex>
+      </Flex>
     </HomePageLayout>
   )
 }
